5391b3c58115cb1443a4d92be258171ad8620bed
Ricardo Geraldes <rgeraldes24@gmail.com>
2020-02-07 01:21:59 +0000

    Added support for uuid as a project id

--- api/resources/serviceaccount/naming.go
+++ api/resources/serviceaccount/naming.go
@@ -34,7 +34,7 @@ var (
 	// EmailPattern is the service account email pattern.
 	EmailPattern = regexp.MustCompile(`^[a-z][-a-z0-9]{4,28}[a-z0-9]@[a-z][-a-z0-9]{3,48}[a-z0-9]\.vserviceaccount\.com$`)
 	// NamePattern is the service account name pattern.
-	NamePattern = regexp.MustCompile(`^projects/(([a-z][-a-z0-9]{3,48}[a-z0-9])|\-)/serviceAccounts/[a-z][-a-z0-9]{4,28}[a-z0-9]@[a-z][-a-z0-9]{3,48}[a-z0-9]\.vserviceaccount\.com$`)
+	NamePattern = regexp.MustCompile(`^projects/(([a-z0-9][-a-z0-9]{3,48}[a-z0-9])|\-)/serviceAccounts/[a-z][-a-z0-9]{4,28}[a-z0-9]@[a-z][-a-z0-9]{3,48}[a-z0-9]\.vserviceaccount\.com$`)
 )
 
 // Name is the service account's name.
