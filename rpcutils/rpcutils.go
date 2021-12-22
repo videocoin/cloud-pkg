5d3e9aa779b18f8746b21b6a3a422aa2a64a70f3
adidenko <aa.didenko@yandex.com>
2021-12-22 23:05:02 +0300

    Update symphony rpc client

--- rpcutils/rpcutils.go
+++ rpcutils/rpcutils.go
@@ -1,28 +1,12 @@
 package rpcutils
 
 import (
-	"context"
-
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 )
 
-func SymphonyRPCClient(symphonyEndpoint, oauthClientID, accessToken string) (*ethclient.Client, error) {
-	c, err := google.JWTConfigFromJSON([]byte(accessToken))
-	if err != nil {
-		return nil, err
-	}
-
-	c.UseIDToken = true
-	c.PrivateClaims = map[string]interface{}{
-		"target_audience": oauthClientID,
-	}
-
-	rpcClient, err := rpc.DialHTTPWithClient(
-		symphonyEndpoint,
-		oauth2.NewClient(context.Background(), c.TokenSource(context.Background())))
+func SymphonyRPCClient(endpoint string) (*ethclient.Client, error) {
+	rpcClient, err := rpc.Dial(endpoint)
 	if err != nil {
 		return nil, err
 	}
